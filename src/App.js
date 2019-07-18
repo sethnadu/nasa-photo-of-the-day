519e88caa90dba404c2e437c61249477bc0ed3cd
Seth Nadu <seth.nadu@gmail.com>
2019-07-18 14:58:27 -0400

    styled buttons and header

--- src/App.js
+++ src/App.js
@@ -3,46 +3,57 @@ import "./Apps.scss";
 import NasaGrid from "./Components/NasaGrid";
 import Header from "./Components/header.js";
 // import Form from "./Components/form.js"
-import Days from "./Components/day.js";
-import Months from './Components/month.js';
-import Years from './Components/year.js';
+import {Days, Months, Year} from "./Components/button.js"
+import styled from "styled-components";
 
 import axios from "axios";
 
 
+const MainContainer = styled.div `
+ text-align: center;
+`;
+
+const ButtonDiv = styled.div `
+  display: flex;
+  justify-content: center;
+  
+
+
+`;
+
+
+
+
+
+
 function App() {
+
  const [nasa, setNasa] = useState([]);
  const [day, setDay] = useState("17");
  const [month, setMonth] = useState("7");
  const [year, setYear] = useState("2019")
 
  useEffect(() => {
-   axios
-    .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${year}-${month}-${day}`)
-    .then((response) => {
-      setNasa(response.data);
+   axios 
+   .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${year}-${month}-${day}`)
+   .then((response) => {
+         setNasa(response.data);
    })
     .catch((error) => {
-     console.log("Something went wrong",error);
+     console.log("Something went wrong",error)
    })
  }, [day, month, year]);
 
- 
-
-
-
-
     return (
-      <div className="App">
-          
+      <MainContainer>
           <Header />
-          <div className = "buttons">
-            <Years   nasa = {nasa} setNasa = {setNasa} year= {year} setYear = {setYear}/>
+          <ButtonDiv>
+            <Year   nasa = {nasa} setNasa = {setNasa} year= {year} setYear = {setYear} />
             <Months  nasa = {nasa} setNasa = {setNasa} month = {month} setMonth = {setMonth}/>
             <Days   nasa = {nasa} setNasa = {setNasa} day = {day} setDay = {setDay}/>
-          </div>
+          </ButtonDiv>
           {<NasaGrid copyright ={nasa.copyright} media_type ={nasa.media_type} date={nasa.date} title = {nasa.title} hdurl = {nasa.hdurl} explanation={nasa.explanation} src={nasa.url}/> }
-      </div>
+      </MainContainer>
     );
 }
 
--- src/Components/NasaGrid.js
+++ src/Components/NasaGrid.js
@@ -2,27 +2,24 @@ import React from "react";
 
 
 
-function NasaGrid(props){
-   return(
-       <>
-           <p className="date">{props.date}</p>
-            <p className="title">{props.title}</p>
 
-           {(props.media_type === "video") ?
-           <iframe title ="content" width="420" height="315"
-           src={props.src}>
-           </iframe>
-           :
-           <img alt = "nasa api " src = {props.hdurl} width = "420" height ="315"/>
-           }
-           <p className="copyright">{props.copyright}</p>
-
-           <p className="explanation">{props.explanation}</p>
-
-
-       </>
-   )
+function NasaGrid(props) {
+  return (
+    <>
+      <p className="date">{props.date}</p>
+        <p className="title">{props.title}</p>
+        
+        {props.media_type === "video" ? (
+        <iframe title="content" width="420" height="315" src={props.src} />
+      ) : (
+        <img alt="nasa api " src={props.hdurl} width="420" height="315" />
+      )}
+      
+        
+         <p className="explanation">{props.explanation}</p>
+       
+    </>
+  );
 }
 
 export default NasaGrid;
-
